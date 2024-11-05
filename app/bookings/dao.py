d0579a16430f7f353bf2b4b5c91deede774670f4
Алексей Задорожный <aleksejzadoroznyj@MacBook-Pro-Aleksej.local>
2024-11-06 00:05:07 +0300

    create dao, refresh methods

---
+++ app/bookings/dao.py
@@ -0,0 +1,16 @@
+from sqlalchemy import select
+
+
+from app.bookings.models import Bookings
+from app.database import async_session_maker
+
+
+class BookingDao:
+
+
+     @classmethod
+     async def find_all(cls):
+         async with async_session_maker() as session:
+            query = select(Bookings)
+            bookings = await session.execute(query)
+            return bookings.mappings().all()
--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 from sqlalchemy import select
 
+from app.bookings.dao import BookingDao
 from app.bookings.models import Bookings
 from app.database import async_session_maker
-
+from app.bookings.dao import BookingDao
 
 router = APIRouter(
      prefix="/bookings",
@@ -11,9 +12,9 @@ router = APIRouter(
 @router.get("")
 async def get_bookings():
     async with async_session_maker() as session:
-        query  = select(Bookings)
-        result = await session.execute(query)
-        return result.mappings().all()
-
+        # query  = select(Bookings.__table__.columns)
+        # result = await session.execute(query)
+        # return result.mappings().all()
+        return await BookingDao.find_all()
 
 
